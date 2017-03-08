dff6b919412d2053646baa995bb959bf69a776b6
Tom Deseyn <tom.deseyn@gmail.com>
2017-03-08 14:58:22 +0100

    Add Transport

---
+++ src/Tmds.Kestrel.Linux/TransportOptions.cs
@@ -0,0 +1,37 @@
+using System;
+
+namespace Tmds.Kestrel.Linux
+{
+    public class TransportOptions
+    {
+        public int ThreadCount { get; set; } = ProcessorThreadCount;
+
+        private static int ProcessorThreadCount
+        {
+            get
+            {
+                // Kestrel implementation
+
+                // Actual core count would be a better number
+                // rather than logical cores which includes hyper-threaded cores.
+                // Divide by 2 for hyper-threading, and good defaults (still need threads to do webserving).
+                var threadCount = Environment.ProcessorCount >> 1;
+
+                if (threadCount < 1)
+                {
+                    // Ensure shifted value is at least one
+                    return 1;
+                }
+
+                if (threadCount > 16)
+                {
+                    // Receive Side Scaling RSS Processor count currently maxes out at 16
+                    // would be better to check the NIC's current hardware queues; but xplat...
+                    return 16;
+                }
+
+                return threadCount;
+            }
+        }
+    }
+}
