a2fb8e93bff430152aac6264e3fd8b95cf3d21e7
Tom Deseyn <tom.deseyn@gmail.com>
2017-03-10 11:53:01 +0100

    Default ThreadCount to twice the number of processors, cfr Netty

--- src/Tmds.Kestrel.Linux/TransportOptions.cs
+++ src/Tmds.Kestrel.Linux/TransportOptions.cs
@@ -10,27 +10,8 @@ namespace Tmds.Kestrel.Linux
         {
             get
             {
-                // Kestrel implementation
-
-                // Actual core count would be a better number
-                // rather than logical cores which includes hyper-threaded cores.
-                // Divide by 2 for hyper-threading, and good defaults (still need threads to do webserving).
-                var threadCount = Environment.ProcessorCount >> 1;
-
-                if (threadCount < 1)
-                {
-                    // Ensure shifted value is at least one
-                    return 1;
-                }
-
-                if (threadCount > 16)
-                {
-                    // Receive Side Scaling RSS Processor count currently maxes out at 16
-                    // would be better to check the NIC's current hardware queues; but xplat...
-                    return 16;
-                }
-
-                return threadCount;
+                // cfr Netty
+                return Environment.ProcessorCount << 1;
             }
         }
     }
